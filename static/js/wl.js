fff30e6607d8a98567f334b604b5ab45c1d4b283
Pranav Kunjir <pranavkunjir93@gmail.com>
2025-02-03 21:04:09 +0530

    add the some freatues to the model still figueing it out though

---
+++ static/js/wl.js
@@ -0,0 +1,9 @@
+
+function get_value(){
+    const selectElement = document.getElementById("day_name"); 
+
+    // Get the selected value
+    const selectedValue = selectElement.value;
+    // Do something with the selected value
+    console.log("Selected value:", selectedValue); 
+}
